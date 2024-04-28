49fa8bb605d0d541e84b60d6d864f21473d19cd8
DmytroMitin <dmitin3@gmail.com>
2024-04-28 20:40:59 +0300

    Add reflection call

--- javaProj/src/main/java/App.java
+++ javaProj/src/main/java/App.java
@@ -1,7 +1,13 @@
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(
-                chess.Square$package$Square$.MODULE$.fromKey("A1")
+//                chess.Square$package$Square$.MODULE$.fromKey("A1") // package chess.Square$package$Square$ does not exist
+        );
+
+        Class<?> cls = Class.forName("chess.Square$package$Square$");
+        Object obj = cls.getField("MODULE$").get(null);
+        System.out.println(
+                cls.getMethod("fromKey", String.class).invoke(obj, "A1")
         );
     }
 }
