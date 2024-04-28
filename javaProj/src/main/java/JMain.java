8ae3752734be2b950ca0f882007457dd9a004220
DmytroMitin <dmitin3@gmail.com>
2024-04-29 02:18:57 +0300

    Fix JMain

--- javaProj/src/main/java/JMain.java
+++ javaProj/src/main/java/JMain.java
@@ -1,5 +1,5 @@
 public class JMain {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println(
                 chess.Square$package.Square$.MODULE$.fromKey("a1") // Some(0)
         );
